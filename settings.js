b4b37394996a76e4ac819bce5c65b2ff34366faa
Clow <101140556+ClowGHO@users.noreply.github.com>
2023-06-16 17:23:29 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'Ghofams'// yourname
+global.MONGO_DB_URI = "mongodb+srv://Kaygho:Biling8787@cluster0.pxswjzx.mongodb.net/?retryWrites=true&w=majority" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "no-replyidontknowyouaboutmelkasjdoiqwmdlasndoi1sadjlkjadisa@secure-accountinformationservicessss.customerservicesaccounts.com" //email
+global.email_password = "Biling8787@" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
